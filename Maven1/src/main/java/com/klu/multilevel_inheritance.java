f37d9a7d4ca018bf4ba2977597d7d4acbb8adf02
Pragathi1256 <2420030672@klh.edu.in>
2026-01-06 09:25:27 +0530

    Add files via upload

---
+++ Maven1/src/main/java/com/klu/multilevel_inheritance.java
@@ -0,0 +1,25 @@
+package com.klu;
+
+class Vehicle {
+void start() {
+	System.out.println("Vehicle Started");
+}
+}
+class Car extends Vehicle{
+	void start() {
+		System.out.println("Driving Car");
+	}
+}
+class ElectricCar extends Vehicle{
+	void start() {
+		System.out.println("Electric Car charged");
+	}
+}
+public class multilevel_inheritance{
+	public static void main(String[] args) { 
+		 Vehicle v1 = new Car();
+	       v1.start();
+	        Vehicle v2 = new ElectricCar();
+	        v2.start();
+	}
+}
